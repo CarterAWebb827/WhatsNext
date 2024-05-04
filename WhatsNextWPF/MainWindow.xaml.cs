b70314ad01990a6e34c11105295b9c00e7336714
CarterAWebb827 <carterawebb827@gmail.com>
2024-05-03 22:14:35 -0700

    BELEIVE IT OR NOT I SPENT 4 HOURS ON THIS BECAUSE AUTOSUGGESTIONBOXES SUCK

--- WhatsNextWPF/MainWindow.xaml.cs
+++ WhatsNextWPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using MaterialDesignThemes.Wpf;
+using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,17 +10,105 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WhatsNextWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _textAnimeTB = "";
+        public string TextAnimeTB
+        {
+            get => _textAnimeTB;
+            set
+            {
+                if (_textAnimeTB != value)
+                {
+                    _textAnimeTB = value;
+                    OnPropertyChanged(nameof(TextAnimeTB));
+                }
+            }
+        }
+
+        private string _textMangaTB = "";
+        public string TextMangaTB
+        {
+            get => _textMangaTB;
+            set
+            {
+                if (_textMangaTB != value)
+                {
+                    _textMangaTB = value;
+                    OnPropertyChanged(nameof(TextMangaTB));
+                }
+            }
+        }
+
+        private bool _isActiveSB = false;
+        public bool IsActiveSB
+        {
+            get => _isActiveSB;
+            set
+            {
+                if (_isActiveSB != value)
+                {
+                    _isActiveSB = value;
+                    OnPropertyChanged(nameof(IsActiveSB));
+                }
+            }
+        }
+
+        private string _messageSB = "";
+        public string MessageSB
+        {
+            get => _messageSB;
+            set
+            {
+                if (_messageSB != value)
+                {
+                    _messageSB = value;
+                    OnPropertyChanged(nameof(MessageSB));
+                }
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        private async void ShowSnackBar(string message)
+        {
+            MessageSB = message;
+            IsActiveSB = true;
+
+            // Simulate a delay before hiding the snackbar
+            await Dispatcher.InvokeAsync(async () =>
+            {
+                await Task.Delay(3000);
+                IsActiveSB = false;
+            });
+        }
+
+        private void TB_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return && !string.IsNullOrWhiteSpace(TextAnimeTB))
+            {
+                ShowSnackBar(TextAnimeTB);
+            }
         }
     }
 }
